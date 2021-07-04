ac8ca09a6025caf36111990a8e6daac6bf654e99
Alex Plescan <alexpls@fastmail.com>
2021-07-04 16:45:50 +1000

    docs: Update examples to use new SDK syntax and provide more instructions

--- examples/express-server/index.js
+++ examples/express-server/index.js
@@ -1,37 +1,44 @@
 const express = require("express");
 const AckloClient = require("@acklo/node-sdk").AckloClient;
 
+// Instantiate the SDK
 const acklo = new AckloClient({
   applicationKey: "[YOUR APPLICATION KEY]",
   environmentName: "local",
 });
 
-acklo
-  .connect()
-  .then(() => console.log("Connected to acklo!"))
-  .catch((err) => console.error("Failed to connect to acklo", err));
+async function main() {
+  // Connect to acklo to retrieve the latest config and stay in sync with
+  // any config updates.
+  await acklo.connect();
 
-const app = express();
+  // Get some config values from acklo.
+  const port = acklo.get("config.port");
+  const messageOfTheDay = acklo.get("config.motd");
 
-const port = acklo.get("config.port");
-const motd = acklo.get("config.motd");
+  const app = express();
 
-app.get("/", (_, res) => {
-  if (acklo.get("switches.maximize_excitement")) {
-    res.json({ motd: `${motd}!!!!!!!!!!!` });
-  } else {
-    res.json({ motd });
-  }
-});
+  app.get("/", (_, res) => {
+    if (acklo.get("switches.maximize_excitement")) {
+      res.json({ motd: `${messageOfTheDay}!!!!!!!!!!!` });
+    } else {
+      res.json({ motd: messageOfTheDay });
+    }
+  });
 
-let server = app.listen(port);
-console.log(`Listening on port ${port}`);
+  let server = app.listen(port);
+  console.log(`Listening on port ${port}`);
 
-acklo.onConfigUpdate((updates) => {
-  if (updates["config.port"]) {
-    const newPort = updates["config.port"].newValue;
-    server.close();
-    server = app.listen(newPort);
-    console.log(`Changed port to ${newPort}`);
-  }
-});
+  acklo.onConfigUpdate((updates) => {
+    if (updates["config.port"]) {
+      const newPort = updates["config.port"].newValue;
+      server.close();
+      server = app.listen(newPort);
+      console.log(`Changed port to ${newPort}`);
+    }
+  });
+}
+
+main()
+  .then(() => console.log("Initialized server"))
+  .catch((err) => console.error(`Error initializing server: ${err}.`));
---
+++ examples/typescript-hello-world/README.md
@@ -0,0 +1,25 @@
+# acklo typescript-hello-world example
+
+This is an example of how to use acklo to remotely manage configuration
+of a Node.js CLI application written in TypeScript.
+
+## Running the example
+
+1. Sign up to [acklo](https://acklo.app) and create an application.
+1. Replace `[YOUR APPLICATION KEY]` with your application's key in `index.ts`.
+1. Install dependencies:
+   ```shell
+   npm install
+   ```
+1. Run the CLI app:
+
+   ```shell
+   yarn start
+
+   # Or
+
+   yarn start Bobby
+   ```
+
+1. Start making changes to the app's config on the acklo UI and you'll see them
+   get applied live!
